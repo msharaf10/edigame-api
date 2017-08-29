2b06eb86ec65ed0491be6387355c489f364be97e
msharaf <msharaf_m10@yahoo.com>
2017-08-29 18:29:30 +0200

    build a skeleton for edigame API
    create package.json and empty basic files

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 const express = require( 'express' );
+
+const app = express();
+
+app.get( '/' , ( req, res, next ) => {
+	res.send( 'test, edigame' );
+});
+
+let server = app.listen( 3000 ),
+	port = server.address().port,
+	mode = app.get( 'env' );
+
+console.log( 'Listening at http://localhost:%s in %s', port, mode );
