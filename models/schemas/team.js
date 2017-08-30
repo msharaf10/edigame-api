6045527722467b5c632615919269b0f8b4e63af7
msharaf <msharaf_m10@yahoo.com>
2017-08-31 00:52:16 +0200

    add team schema

--- models/schemas/team.js
+++ models/schemas/team.js
@@ -4,6 +4,35 @@ const Schema = mongoose.Schema;
 
 const teamSchema = new Schema({
 	// TODO complete team schema
+    teamName: { type: String, require: true, unique: true, trim: true },
+    companyName: String,
+    room: { type: mongoose.Schema.Types.ObjectId, ref: 'Room' },
+    progress: {},
+    status: {},
+    players: [{
+        playerId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+        isLeader: Boolean,
+        role: String,
+        equipments: {
+            personalGear: {},
+            climbingGear: {},
+            medicalGear: {},
+            clothing: {}
+        },
+        role: { type: String, unique: true, trim: true },
+        health: Number,
+        level: Number,
+        decisions: [{}]
+    }],
+    started: Boolean,
+    finished: Boolean,
+},
+{
+	toObject: { getters: true },
+	timestamps: {
+		createdAt: 'createdDate',
+		updatedAt: 'updatedDate'
+	}
 });
 
 // TODO create pre(save) hooks
