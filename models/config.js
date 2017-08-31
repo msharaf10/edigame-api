d1606f0782a5382c9a39a36f826650f5e24b8cee
msharaf <msharaf_m10@yahoo.com>
2017-08-31 23:54:55 +0200

    add regex, secretKey & db url to config file, add jwt & bcrypt packages

--- models/config.js
+++ models/config.js
@@ -1,4 +1,6 @@
 module.exports = {
 	port: 3000,
-	dbURL: 'localhost:5000'
+	dbURL: 'localhost:5000',
+	secretKey: 'thisdefinitelynotthesecretkey',
+	regex: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 };
