89a03c7dee21bff1c2daee4cea18da2457d80716
msharaf10 <msharaf_m10@yahoo.com>
2017-10-30 00:26:29 +0200

    add new routes and organize REST API

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
+/*
+*
+*  ->  FILENAME :    index.js
+*
+*  ->  RESPONSIBILITY :
+*			Server container file
+*
+*  -> TO GET STARTED :
+*			run `npm start`
+*
+*  ->  AUTHOR :		Mohamed Sharaf
+*  ->  EMAIL :		mohamedsharafm10@gmail.com
+*
+*  ->  LAST MODIFIED BY :
+*			Mohamed Sharaf
+*
+*/
+
 const express = require( 'express' );
 const logger = require( 'morgan' );
 const bodyParser = require( 'body-parser' );
 const mongoose = require( 'mongoose' );
 
-const db = require( './config/db' );
-const server_config = require( './config/server' );
+const { server, db } = require( './config/main' );
 
-const routes = require( './routes/routes' );
+const { routes, validateIDs } = require( './routes/routes' );
 
 const app = express();
 
 mongoose.Promise = global.Promise;
-mongoose.connect( db.dbURL, { server: { socketOptions: { keepAlive: 120 } } } );
+mongoose.connect( `mongodb://${ db.dbURL }`, {
+	useMongoClient: true,
+	keepAlive: 300000
+}).catch( err => console.error( err.message ) );
 
 if ( app.get( 'env' ) !== 'production' ) app.use( logger( 'dev' ) );
 else require( './init/init' ); // run init script if in production mode
@@ -19,6 +39,7 @@ else require( './init/init' ); // run init script if in production mode
 app.use( bodyParser.json() );
 app.use( bodyParser.urlencoded( { extended: false } ) );
 
+app.use( validateIDs )
 app.use( '/', routes );
 
 // =====================================
@@ -31,22 +52,22 @@ app.use( ( req, res, next ) => {
 });
 
 app.use( ( err, req, res, next ) => {
-	if ( app.get( 'env' ) === 'development' ) console.log( err.message );
+	if ( app.get( 'env' ) === 'development' ) console.error( err.message );
 
 	let status = err.status || 500;
 	let message;
 
-	if ( status >= 400 && status < 500 && err.message ) {
+	if ( status >= 400 && status < 500 && err.message )
 		message = err.message;
-	} else {
+	else
 		message = '';
 
-	}
-	res.status( status ).send( message );
+
+	res.status( status ).json( { error: message } );
 });
 
-let server = app.listen( server_config.port ),
-	port = server.address().port,
+let SERVER = app.listen( server.port ),
+	port = SERVER.address().port,
 	mode = app.get( 'env' );
 
-console.log( 'Listening at http://localhost:%s in %s', port, mode );
+console.log( `Listening at http://localhost:${ port } in ${ mode }` );
